1794a5a39013c5e20fe41a9785b697d524d97373
John Lee <john.lee@wfs.aero>
2021-06-23 16:58:20 -0400

    first commit

---
+++ LambdaForums.Data/IForum.cs
@@ -0,0 +1,18 @@
+﻿using LambdaForums.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
